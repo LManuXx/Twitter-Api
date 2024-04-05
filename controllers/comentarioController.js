1342f066de9ca9e15f50ea6673d272db4a36849d
LManuXx <manuel.nto.rmo@gmail.com>
2024-04-05 09:34:52 +0200

    Comentarios terminados y comprobados

--- controllers/comentarioController.js
+++ controllers/comentarioController.js
@@ -4,7 +4,7 @@ const Tweet = require('../models/Tweet.js')
 exports.obtenerComentariosPorTweet = async (req, res) => {
   try {
     const tweetId = req.params.tweetId
-    const comentarios = await Comentario.find({ tweet: tweetId }).populate('autor')
+    const comentarios = await Comentario.find({ tweet: tweetId })
     res.json(comentarios)
   } catch (error) {
     res.status(500).json({ mensaje: 'Error al obtener los comentarios del tweet', error: error.message })
@@ -13,8 +13,8 @@ exports.obtenerComentariosPorTweet = async (req, res) => {
 
 exports.obtenerComentarioPorId = async (req, res) => {
   try {
-    const comentarioId = req.params.id
-    const comentario = await Comentario.findById(comentarioId).populate('autor')
+    const comentarioId = req.params.comentarioId
+    const comentario = await Comentario.findById(comentarioId)
     if (!comentario) {
       return res.status(404).json({ mensaje: 'Comentario no encontrado' })
     }
@@ -25,34 +25,35 @@ exports.obtenerComentarioPorId = async (req, res) => {
 }
 
 exports.crearNuevoComentario = async (req, res) => {
-  const { contenido } = req.body;
-  const usuarioId = req.usuario.id;
-  const tweetId = req.params.tweetId;
+  const { contenido } = req.body
+  const usuarioId = req.usuario.id
+  const tweetId = req.params.tweetId
 
   try {
-    // Crear el nuevo comentario
+    
     const nuevoComentario = new Comentario({
       contenido,
       autor: usuarioId,
       tweet: tweetId
     });
-    await nuevoComentario.save();
+    await nuevoComentario.save()
 
-    // Agregar el comentario al array de comentarios en el tweet correspondiente
-    await Tweet.findByIdAndUpdate(tweetId, { $push: { comentarios: nuevoComentario._id } });
+    
+    await Tweet.findByIdAndUpdate(tweetId, { $push: { comentarios: nuevoComentario._id } })
 
-    res.status(201).json({ mensaje: 'Comentario creado correctamente', comentario: nuevoComentario });
+    res.status(201).json({ mensaje: 'Comentario creado correctamente', comentario: nuevoComentario })
   } catch (error) {
-    res.status(500).json({ mensaje: 'Error al crear el comentario', error: error.message });
+    res.status(500).json({ mensaje: 'Error al crear el comentario', error: error.message })
   }
 };
 
 exports.actualizarComentario = async (req, res) => {
-  const comentarioId = req.params.id
+  const comentarioId = req.params.comentarioId
   const { contenido } = req.body
   const usuarioId = req.usuario.id
 
   try {
+
     const comentario = await Comentario.findById(comentarioId)
     if (!comentario) {
       return res.status(404).json({ mensaje: 'Comentario no encontrado' })
@@ -70,7 +71,7 @@ exports.actualizarComentario = async (req, res) => {
 
 
 exports.eliminarComentarioPorId = async (req, res) => {
-  const comentarioId = req.params.id
+  const comentarioId = req.params.comentarioId
   const usuarioId = req.usuario.id
 
   try {
@@ -81,7 +82,7 @@ exports.eliminarComentarioPorId = async (req, res) => {
     if (comentario.autor.toString() !== usuarioId) {
       return res.status(403).json({ mensaje: 'No tienes permiso para eliminar este comentario' })
     }
-    await comentario.remove()
+    await Comentario.deleteOne({_id:comentario._id})
     res.json({ mensaje: 'Comentario eliminado correctamente' })
   } catch (error) {
     res.status(500).json({ mensaje: 'Error al eliminar el comentario', error: error.message })
--- routes/comentarioRoutes.js
+++ routes/comentarioRoutes.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const comentarioController = require('../controllers/comentarioController.js');
-const { verificarTokenMw } = require('../middleware/authMiddleware.js');
+const express = require('express')
+const router = express.Router()
+const comentarioController = require('../controllers/comentarioController.js')
+const { verificarTokenMw } = require('../middleware/authMiddleware.js')
 
-// Obtener todos los comentarios de un tweet
-router.get('/:tweetId', verificarTokenMw,comentarioController.obtenerComentariosPorTweet);
+router.get('/:tweetId', verificarTokenMw,comentarioController.obtenerComentariosPorTweet)
 
-// Obtener un comentario por su ID
-router.get('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.obtenerComentarioPorId);
+router.get('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.obtenerComentarioPorId)
 
-// Crear un nuevo comentario para un tweet
-router.post('/:tweetId', verificarTokenMw,comentarioController.crearNuevoComentario);
+router.post('/:tweetId', verificarTokenMw,comentarioController.crearNuevoComentario)
 
-// Actualizar un comentario existente
-router.put('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.actualizarComentario);
+router.put('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.actualizarComentario)
 
-// Eliminar un comentario por su ID
-router.delete('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.eliminarComentarioPorId);
+router.delete('/:tweetId/:comentarioId', verificarTokenMw,comentarioController.eliminarComentarioPorId)
 
 module.exports = router;
